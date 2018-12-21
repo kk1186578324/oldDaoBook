881c97fedb30ceed773984b424d5b6929c747486
kai.liu <kai.liu@tellyes.com>
2018-12-21 19:08:19 +0800

    v3

--- components/epsoide/epsoide.js
+++ components/epsoide/epsoide.js
@@ -11,6 +11,11 @@ Component({
    * 组件的初始数据
    */
   data: {
+    message:"111", 
+    view:"MINA",
+    count:1,
+    id:0,
+    zero:0
 
   },
 
@@ -18,6 +23,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
+
+     add(e){
+  
+      // this.count = this.count+1;
+
+       this.setData({
+           count:this.data.count +1
+
+
+       })
+     }
+    
 
   }
 })
--- components/like/index.js
+++ components/like/index.js
@@ -4,6 +4,9 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    like:{
+      type:Number
+    }
 
   },
 
@@ -11,6 +14,9 @@ Component({
    * 组件的初始数据
    */
   data: {
+    count:998,
+    count1:"",
+    imgstatus:1
 
   },
 
@@ -18,6 +24,20 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onLike(){
+      var count = this.data.count+1;
+      var count1=""
+      if (count>=1000){
+        count1 = Math.floor((count/1000)*10)/10+'k'
+      }
+      console.log(count)
+      this.setData({
+        count: this.data.count+1,
+        count1: count1,
+        imgstatus: this.data.imgstatus == 1 ? 2 : 1
+
+      })
+    }
 
   }
 })
